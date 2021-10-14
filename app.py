9489568b079fe2e6395ba41c189c2603e790ef78
JWUZYK <jan.wuzyk@gmail.com>
2021-10-14 13:37:12 +0200

    Added failed loading behaviour

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from scheduling_app import JobSchedulerScreen
-from forecasting_app import ForecastScreen
-from planning_app import PlanningScreen
-from routing_app import RoutingScreen
-from queue_app import QueueScreen
+
 import streamlit as st
 st.set_page_config(layout="wide")
+class EmptyScreen:
+
+    def show():
+        st.markdown("Failed to Load")
+
+try:
+    from scheduling_app import JobSchedulerScreen
+except:
+    JobSchedulerScreen = EmptyScreen
+
+try:
+    from forecasting_app import ForecastScreen
+except:
+    ForecastScreen = EmptyScreen
+    print("Failed to import:")
+
+try:
+    from planning_app import PlanningScreen
+except:
+    PlanningScreen = EmptyScreen
+    print("Failed to import:")
+
+try:
+    from routing_app import RoutingScreen
+except:
+    RoutingScreen = EmptyScreen
+    print("Failed to import:")
+
+try:
+    JobSchedulerScreen = EmptyScreen
+    from queue_app import QueueScreen
+except:
+    print("Failed to import:")
 
 screens = {
     "Scheduling": JobSchedulerScreen,
